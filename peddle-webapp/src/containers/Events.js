9b8de89976bf9cbd00aa1d724b176c72b9bc25a5
Александр Копин <kopin.alex@gmail.com>
2018-08-12 16:25:06 +0300

    -add react router to App -add main menu to App -create folder structure and containers skeleton

---
+++ peddle-webapp/src/containers/Events.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Events extends Component{
+  render(){
+    return(
+        <div>
+          <h1>Events is hire!</h1>
+        </div>
+
+    );
+  }
+}
+export default Events;
