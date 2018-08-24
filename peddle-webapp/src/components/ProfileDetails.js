4d20b97c48c3f740a7ec87a715e6eb264c6f37ca
Paul Rubchinsky <pauldeusx@gmail.com>
2018-08-24 20:31:38 +0300

    - Add ProfileDetails component (this.state values)

--- peddle-webapp/src/components/ProfileDetails.js
+++ peddle-webapp/src/components/ProfileDetails.js
@@ -1,30 +1,19 @@
 import React, {Component} from 'react';
 //import dataMap from '../constants/ApiSettings';
-//kimport { connect } from 'react-redux';
+//import { connect } from 'react-redux';
 
 
 class ProfileDetails extends Component {
-
-    constructor() {
-        super();
-        this.state = {
-            username: "Bobby Axelrod",
-            user_email: "baxe@gmail.com",
-            user_location: "New York/NY/USA",
-            u_id: 1,
-        }
-    };
-
     render() {
-        const {username, user_location, user_email} = this.state;
             return (
                 <div>
-                    <p>Name: {username}</p>
-                    <p> Email: {user_email}</p>
-                    <p>Location: {user_location}</p>
+                    <p>Name: {this.props.User.username}</p>
+                    <p> Email: {this.props.User.user_email}</p>
+                    <p>Location: {this.props.User.user_location}</p>
                 </div>
             );
         }
 
 }
+
 export default ProfileDetails;
--- peddle-webapp/src/containers/Profile.js
+++ peddle-webapp/src/containers/Profile.js
@@ -1,20 +1,39 @@
 import React,{Component} from 'react';
+import { render } from 'react-dom';
 import ProfileDetails from "../components/ProfileDetails";
 
 
 class Profile extends Component{
+    constructor() {
+        super();
+        this.state = ({
+            User: {
+                u_id: 1,
+                username: "Bobby Axelrod",
+                user_email: "baxe@gmail.com",
+                user_location: "New York/NY/USA"
+            }
+        })
+    };
 
   render(){
     return(
         <div>
-        <h1>
+
+        <p>
             Profile details
-        </h1>
-            <ProfileDetails/>
+        </p>
+            <ProfileDetails
+                u_id={this.state.u_id}
+                username={this.state.username}
+                user_email={this.state.user_email}
+                user_location= {this.state.user_location}
+            />
+
         </div>
 
     )
   }
 }
-
+render(<Profile />, document.getElementById('root'));
 export default Profile;
