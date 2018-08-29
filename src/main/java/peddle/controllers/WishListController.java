32e6facac2846f07c43739083cdb4428dfb738a5
Volodymyr <genius777555@gmail.com>
2018-08-29 17:40:24 +0300

    Wishlist-new - the first step

--- src/main/java/peddle/controllers/WishListController.java
+++ src/main/java/peddle/controllers/WishListController.java
@@ -1,7 +1,12 @@
 package peddle.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import peddle.dto.EventDtoRs;
 import peddle.services.WishListService;
 
