0393cf8c2a865c3bf94e766ca4143a8029ef8521
Александр Копин <kopin.alex@gmail.com>
2018-09-11 17:55:03 +0300

    add choose city option to purchase

--- peddle-webapp/src/actions/transferActions.js
+++ peddle-webapp/src/actions/transferActions.js
@@ -1,4 +1,16 @@
-import {SET_CITY_FOR_TRANSFER_FROM_EVENT, SET_CITY_FOR_TRANSFER_TO_EVENT} from "./actionsTypes";
+import {
+  SET_CITY_FOR_TRANSFER_FROM_EVENT,
+  SET_CITY_FOR_TRANSFER_TO_EVENT,
+  SET_DATE_TRANSFER_FROM_EVENT,
+  SET_DATE_TRANSFER_TO_EVENT,
+  SET_EVENT_CITY
+} from "./actionsTypes";
+
+export function setEventCity(city) {
+  return{
+    type: SET_EVENT_CITY, city
+  }
+}
 
 export function setCityForTransferToEvent(city) {
   return{
@@ -9,5 +21,17 @@ export function setCityForTransferToEvent(city) {
 export function setCityForTransferFromEvent(city) {
   return{
     type: SET_CITY_FOR_TRANSFER_FROM_EVENT, city
+  }
+}
+
+export function setDatesForTransferToEvent(date1, date2) {
+  return{
+    type: SET_DATE_TRANSFER_TO_EVENT, date1, date2
+  }
+}
+
+export function setDatesForTransferFromEvent(date1, date2) {
+  return{
+    type: SET_DATE_TRANSFER_FROM_EVENT, date1, date2
   }
 }
--- peddle-webapp/src/reducers/transferReducer.js
+++ peddle-webapp/src/reducers/transferReducer.js
@@ -2,26 +2,31 @@ import {
   SET_CITY_FOR_TRANSFER_FROM_EVENT,
   SET_CITY_FOR_TRANSFER_TO_EVENT,
   SET_DATE_TRANSFER_FROM_EVENT,
-  SET_DATE_TRANSFER_TO_EVENT
+  SET_DATE_TRANSFER_TO_EVENT, SET_EVENT_CITY
 } from "../actions/actionsTypes";
 
 const initialState = {
-  cityTransferToEvent: '',
-  cityTransferFromEvent: '',
-  dateTransferToEvent: '',
-  dateTransferFromEvent: ''
-}
+  eventCity:'',
+  cityTransferDepartToEvent: '',
+  cityTransferArrivalFromEvent: '',
+  dateTransferToEvent1: '',
+  dateTransferToEvent2: '',
+  dateTransferFromEvent1: '',
+  dateTransferFromEvent2: ''
+};
 
 function transferReducer(state = initialState, action) {
   switch (action.type) {
     case SET_CITY_FOR_TRANSFER_TO_EVENT:
-      return {cityTransferToEvent: action.city};
+      return {...state, cityTransferDepartToEvent: action.city};
+    case SET_EVENT_CITY:
+      return {...state, eventCity: action.city};
     case SET_CITY_FOR_TRANSFER_FROM_EVENT:
-      return {cityTransferFromEvent: action.city}
+      return {...state, cityTransferArrivalFromEvent: action.city};
     case SET_DATE_TRANSFER_TO_EVENT:
-      return {dateTransferToEvent: action.transferDate};
+      return {...state, dateTransferToEvent1: action.date1, dateTransferToEvent2: action.date2};
     case SET_DATE_TRANSFER_FROM_EVENT:
-      return {dateTransferToEvent: action.transferDate};
+      return {...state, dateTransferFromEvent1: action.date1, dateTransferFromEvent2: action.date2};
     default:
       return state;
   }
