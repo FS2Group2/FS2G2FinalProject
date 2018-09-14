5cca5ff2f775e10c727617dc9219386f09300386
Paul Rubchinsky <pauldeusx@gmail.com>
2018-09-14 17:01:08 +0300

    - Try to render purchase fix

--- peddle-webapp/src/components/MyPurchases.js
+++ peddle-webapp/src/components/MyPurchases.js
@@ -3,7 +3,7 @@ import '../css/profile.css'
 import "../constants/ApiSettings.js"
 import {connect} from "react-redux";
 import {loadPurchaceList} from "../actions/purchaceActions";
-import dataMap, {authHeaders} from "../constants/ApiSettings";
+import dataMap from "../constants/ApiSettings";
 
 class MyPurchases extends Component {
 
@@ -47,53 +47,26 @@ class MyPurchases extends Component {
                 </div>
             </ul>
         )
+        ///******
     }
-    
-  loadPurchaseList() {
-    let reqParam = {
-      method: 'GET',
-      headers: authHeaders
-    };
-    const url = dataMap.purchace + this.props.userState.currentUser.id;
-    console.log('request params:' + JSON.stringify(reqParam));
-    fetch(url, reqParam)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.props.loadPurchaseListToStore(result)
-        })
-  }
-
-  componentDidMount() {
-    this.loadPurchaseList();
-  }
-
-  render() {
-    return (
-      <ul>
-        <li>My purchases:</li>
-      </ul>
-    );
-  }
-
 }
 
 
 
 const mapStateToProps = (state) => {
-  return {
-    userState: state.userReducer,
-    purchase: state.purchaseReducer
-  }
+    return {
+        userState: state.userReducer,
+        purchase: state.purchaseReducer
+    }
 };
 
 const mapDispatchToProps = dispatch => {
-  return {
-    loadPurchaseListToStore: data => {
-      dispatch(loadPurchaceList(data))
-    }
+    return {
+        loadPurchaseListToStore: data => {
+            dispatch(loadPurchaceList(data))
+        }
 
-  }
+    }
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(MyPurchases);
