9f218b03e9a050d990fa43f4e28ff3d8f36c6fc0
Александр Копин <kopin.alex@gmail.com>
2018-09-17 13:55:36 +0300

    add category icons

--- peddle-webapp/src/constants/ApiSettings.js
+++ peddle-webapp/src/constants/ApiSettings.js
@@ -9,6 +9,7 @@ const dataPath = apiSettings.apiPath;
 
 export const eventImgPath = '/img/';
 export const categoryImgPath = '/img/categories/';
+export const categoryIconPath = '/img/categories/icons/';
 export const userPhotoPath = '/img/users/';
 
 const dataMap = {
