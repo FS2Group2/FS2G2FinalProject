1c1e29cfcf36806be86fbe19aacc13587f6e546c
fs2danitpavel <mahntest322@gmail.com>
2018-09-18 15:50:44 +0300

    - Add wishlist css; - purchase css fix;

--- peddle-webapp/src/components/Wishlist.js
+++ peddle-webapp/src/components/Wishlist.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import '../css/profile.css'
+import '../css/Wishlist.css'
 import {connect} from "react-redux";
 import {Link} from "react-router-dom";
 
@@ -8,15 +8,45 @@ class Wishlist extends Component {
     render() {
         const {wishListState} = this.props;
         return (
-            <div>
+            <div className="wishlist-list">
+                <div className="wishlist-list-titles">
+
+                    <div className="element-title-name">
+                        <span >Name</span>
+                    </div>
+
+                    <div className="element-title-city">
+                        <span >City</span>
+                    </div>
+
+                    <div className="element-title-price">
+                        <span >$</span>
+                    </div>
+
+                    <div className="element-title-link">
+                        <span >Link</span>
+                    </div>
+
+                </div>
                 {wishListState[0] && wishListState.map(w =>
-                    <div>
-                        <Link to={'event/' + w.id}>
-                            <p>{w.name}</p>
-                            <p>{w.cityName}</p>
-                            <p>{w.date}</p>
-                            <p>  ___ </p>
-                        </Link>
+                    <div className="wishlist-list-element">
+
+                        <div className="element-name">
+                            <span >{w.name}</span>
+                        </div>
+
+                        <div className="element-city">
+                            <span >{w.cityName}</span>
+                        </div>
+
+                        <div className="element-sum">
+                            <span >{w.price}</span>
+                        </div>
+
+                        <div className="element-sum">
+                            <Link to={'event/' + w.id}><span>View Event</span></Link>
+                        </div>
+
                     </div>
                 )}
             </div>
