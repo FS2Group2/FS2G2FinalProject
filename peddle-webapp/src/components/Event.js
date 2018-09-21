2daee8a0b39cc54474e322d9ce9e4f0393ae2856
Александр Копин <kopin.alex@gmail.com>
2018-09-21 20:30:19 +0300

    add "favorite" icon to event card

--- peddle-webapp/src/components/Event.js
+++ peddle-webapp/src/components/Event.js
@@ -1,29 +1,41 @@
 import React, {Component} from 'react';
 import '../css/event.css';
-import {categoryIconPath, eventImgPath} from '../constants/ApiSettings'
+import {categoryIconPath, eventImgPath, iconPath} from '../constants/ApiSettings'
+import {connect} from "react-redux";
 
 class Event extends Component {
   render() {
 
     const e = this.props.theEvent;
+    const wishlist = this.props.wishList;
+
+    let inWishes = wishlist.some(w => w.id === e.id);
+
     let eventDate = new Date(e.date);
-    let imgPath = eventImgPath+e.eventExtraPhoto;
-    let iconPath = `${categoryIconPath + e.categoryName.replace(/ /g, '_').toLowerCase()}.svg`;
+    let imgPath = eventImgPath + e.eventExtraPhoto;
+    let iconCatPath = `${categoryIconPath + e.categoryName.replace(/ /g, '_').toLowerCase()}.svg`;
 
 
     return (
-        <div className='event-item'>
-          <h2 className='event-item-name'>{e.name}</h2>
-          <img src={iconPath} className={'icon'} alt="" title={e.categoryName}/>
-          <div className='event-item-img-container'>
-            <img className='event-item-img' src={imgPath} alt="event-img"/>
-          </div>
-          <p className='event-item-date'>{eventDate.toLocaleDateString()}</p>
-          <p className='event-item-city'>{e.cityName}</p>
-          <h3 className='event-item-price'>${e.price}</h3>
+      <div className='event-item'>
+        <h2 className='event-item-name'>{e.name}</h2>
+        <img src={iconCatPath} className={'icon'} alt="" title={e.categoryName}/>
+        <div className='event-item-img-container'>
+          <img className='event-item-img' src={imgPath} alt="event-img"/>
+          {inWishes && <img src={iconPath + 'star_full.svg'} className={'star-icon-event'} alt=""/>}
         </div>
+        <p className='event-item-date'>{eventDate.toLocaleDateString()}</p>
+        <p className='event-item-city'>{e.cityName}</p>
+        <h3 className='event-item-price'>${e.price}</h3>
+      </div>
     );
   }
 }
 
-export default Event;
+const mapStateToProps = (state) => {
+  return {
+    wishList: state.wishListReducer
+  }
+};
+
+export default connect(mapStateToProps)(Event);
