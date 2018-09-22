97fdac2f046e4be3fb07b8c4249ab0eb6bf0e829
Paul Rubchinsky <pauldeusx@gmail.com>
2018-09-22 12:05:46 +0300

    - Add tab Links (stage 1)

--- peddle-webapp/src/containers/Profile.js
+++ peddle-webapp/src/containers/Profile.js
@@ -5,6 +5,22 @@ import {Link, Route, Switch} from "react-router-dom";
 import MyPurchases from "../components/MyPurchases";
 import Wishlist from "../components/Wishlist";
 
+function openCity(evt, Name) {
+    // Declare all variables
+    var i, tablinks;
+
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active-link", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(Name).style.display = "block";
+    evt.currentTarget.className += " active-link";
+}
+
 class Profile extends Component {
 
     render() {
@@ -18,11 +34,13 @@ class Profile extends Component {
             :
         <div>
           <ProfileDetails user={user}/>
-          <div className="lists-container">
-            <nav>
-              <Link to={path}><div className='link-to-list'>My wishlist </div></Link>
-              <Link to={path + '/purchases'}><div className='link-to-list'>My purchases </div></Link>
-            </nav>
+          <div className="tab">
+              <Link to={path} className='link-to-list'>
+                  <div className="tablink active-link" onClick={openCity(event, 'My wishlist')}>My wishlist </div>
+              </Link>
+              <Link to={path + '/purchases'} className="link-to-list">
+                  <div className="tablink" onClick={openCity(event,'My purchase')}>My purchases </div>
+              </Link>
           </div>
           <Switch>
             <Route exact path={path} component={Wishlist}/>
