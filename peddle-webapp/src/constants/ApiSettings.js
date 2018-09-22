d05ff5c66edb93058e3d62ee4fe7a5cb654483aa
Yuriy <ch.yuriy@ukr.net>
2018-09-22 18:48:24 +0300

    Fixed login

--- peddle-webapp/src/constants/ApiSettings.js
+++ peddle-webapp/src/constants/ApiSettings.js
@@ -18,7 +18,7 @@ const dataMap = {
   allCities: dataPath + '/city/all',
   event: dataPath + '/events/info/',
   user: dataPath + '/user',
-  login: dataPath + '/user/login',
+  login: dataPath + '/login',
   filterEvents: dataPath + '/events/filter/',
   accommodations: dataPath + '/accommodations/city/',
   transfer: dataPath + '/transfer',
