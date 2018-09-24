0f79a4677360ab554956f46c9aee3496169f466f
Александр Копин <kopin.alex@gmail.com>
2018-09-24 20:52:38 +0300

    registration front

--- peddle-webapp/src/containers/RegisterPage.js
+++ peddle-webapp/src/containers/RegisterPage.js
@@ -1,27 +1,89 @@
-import React,{Component} from 'react';
-import { Link } from 'react-router-dom';
-import  '../css/Register.css'
+import React, {Component, Fragment} from 'react';
+import {Link} from 'react-router-dom';
+import '../css/Register.css'
+import {fetchRegister} from "../actions/userActions";
+import {connect} from "react-redux";
 
 
 class RegisterPage extends Component {
-    render(){
-        return(
-            <div className="register-box">
-                <div className="register-box-header">
-                    <p className="register-box-header-left">Sign Up</p>
-                </div>
-                <input className="register-input-box" type="text" name="login" placeholder="You Name"/>
-                       {/*value={this.state.login} onChange={this.onLoginChange}/>*/}
-                <input className="register-input-box" type="email" name="Email" placeholder="Email"/>
-                <input className="register-input-box" type="password" name="pass1" placeholder="Choose Password"/>
-                <input className="register-input-box" type="password" name="pass2" placeholder="Confirm Password"/>
-                       {/*// value={this.state.password} onChange={this.onPasswordChange}/>*/}
-                <input className="reg-btn" type="button" value="Register"/>
-                       {/*// onClick={this.goLogin} />*/}
-                <Link to="/login" className="register-login-link">Login</Link>
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      password1: '',
+      password2: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const {name, value} = event.target;
+    this.setState({[name]: value});
+  };
+
+  handleSubmit(event) {
+    let {name, email, password1, password2} = this.state;
+    let registrationData = {
+      name: name,
+      email: email,
+      password: password1
+    };
+    event.preventDefault();
+    this.props.sendRegistrationData(registrationData)
+  };
+
+  render() {
+    const {regState} = this.props;
+    let {name, email, password1, password2} = this.state;
+
+    return (
+      <Fragment>
+        {regState.message.success ? <div className="register-box">
+            <div className="register-message">
+              <p className="success-msg-p">{regState.message.message}</p>
             </div>
-        )
-    }
+          </div> :
+          <div className="register-box">
+            <div className="register-box-header">
+              <p className="register-box-header-left">Sign Up</p>
+            </div>
+            <input className="register-input-box" type="text" name="name" placeholder="You Name"
+                   value={name} onChange={this.handleChange}/>
+            <input className="register-input-box" type="email" name="email" placeholder="Email"
+                   value={email} onChange={this.handleChange}/>
+            <input className="register-input-box" type="password" name="password1" placeholder="Choose Password"
+                   value={password1} onChange={this.handleChange}/>
+            <input className="register-input-box" type="password" name="password2" placeholder="Confirm Password"
+                   value={password2} onChange={this.handleChange}/>
+            <input className="reg-btn" type="button" value="Register"
+                   onClick={this.handleSubmit}/>
+            {(regState.registerError || regState.message.success) && <div className="register-message">
+              <p className="err-msg-p">{regState.registerError.massage || regState.registerError.message ||
+              regState.message.message}</p>
+            </div>}
+            <Link to="/login" className="register-login-link">Login</Link>
+          </div>}
+      </Fragment>
+
+    )
+  }
 }
 
-export default RegisterPage;
+const
+  mapStateToProps = (state) => {
+    return {
+      regState: state.userReducer
+    }
+  };
+
+const
+  mapDispatchToProps = (dispatch) => {
+    return {
+      sendRegistrationData: (registrationData) => dispatch(fetchRegister(registrationData))
+    }
+  };
+
+export default connect(mapStateToProps, mapDispatchToProps)(RegisterPage);
