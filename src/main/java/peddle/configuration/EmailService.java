b4613df05d67d66bf41c9658c3758055e9346c95
Yuriy Chechuga <yuriy.chechuga@gmail.com>
2018-10-01 14:38:51 +0300

    Added prepareAndSend method to MailService

--- src/main/java/peddle/configuration/EmailService.java
+++ src/main/java/peddle/configuration/EmailService.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 @PropertySource("config.properties")
@@ -17,6 +18,9 @@ public class EmailService {
 
   @Autowired
   private JavaMailSender emailSender;
+
+  @Autowired
+  private MailContentBuilder mailContentBuilder;
 
   public void sendSimpleMessage(
       String to, String subject, String text) {
@@ -30,4 +34,14 @@ public class EmailService {
     emailSender.send(message);
   }
 
+  public void prepareAndSend(String to, String subject, String text) {
+    SimpleMailMessage message = new SimpleMailMessage();
+    message.setFrom(sendersMail);
+    message.setTo(to);
+    message.setSubject(subject);
+    String content = mailContentBuilder.build(text);
+//    System.out.println(content);
+    message.setText(content);
+    emailSender.send(message);
+  }
 }
