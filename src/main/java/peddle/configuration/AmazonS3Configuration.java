adc2b9df49d2511cbbd2eb28543334baff285b92
Volodymyr <genius777555@gmail.com>
2018-10-05 12:48:39 +0300

    Amazon - first step

---
+++ src/main/java/peddle/configuration/AmazonS3Configuration.java
@@ -0,0 +1,31 @@
+package peddle.configuration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@PropertySource("config.properties")
+@Component
+public class AmazonS3Configuration {
+
+  @Value("${Access_key}")
+  private String accessKey;
+
+  @Value("${Secret_key}")
+  private String secretKey;
+
+  public static final String BUCKET_NAME = "peddle-bucket";
+
+  public AmazonS3 getAmazonS3() {
+    return AmazonS3ClientBuilder.standard()
+            .withCredentials(new AWSStaticCredentialsProvider(
+                    new BasicAWSCredentials(accessKey,
+                            secretKey)))
+            .withRegion(Regions.US_EAST_1) .build() ;
+  }
+}
