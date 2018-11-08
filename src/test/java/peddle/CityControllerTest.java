2ee28cbb29896a6b3f3ea117b9c87341325f9b84
Yuriy <ch.yuriy@ukr.net>
2018-11-08 02:20:05 +0200

    Fixed test CityController

--- src/test/java/peddle/CityControllerTest.java
+++ src/test/java/peddle/CityControllerTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import peddle.controllers.CityController;
 import peddle.dto.CityDto;
 import peddle.services.CityServiceImpl;
@@ -23,14 +22,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-//import static org.mockito.BDDMockito.given;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @RunWith(SpringRunner.class)
-//@RunWith(MockitoJUnitRunner.class)
 @WebMvcTest(CityController.class)
 @AutoConfigureMockMvc(secure = false)
 public class CityControllerTest {
@@ -56,7 +48,7 @@ public class CityControllerTest {
         .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk())
         .andExpect(jsonPath("$", hasSize(1)))
-        .andExpect(jsonPath("$[0].name", is(CITY_NAME)));
+        .andExpect(jsonPath("$[0].name").value(CITY_NAME));
   }
 
 }
